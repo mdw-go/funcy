df1dbc8d7c58469a5b4fb572bef9e9e4f6a4ec06
Michael Whatcott <mike@smarty.com>
2023-01-19 10:56:39 -0700

    Convenient (but risky) helper.

---
+++ risky/risky.go
@@ -0,0 +1,10 @@
+// Package risky houses convenience functions that introduce runtime risks (i.e. cavalier use of reflection)
+// and therefore are separated from the root funcy package.
+package risky
+
+import "reflect"
+
+// Field provides a func() for convenient use with funcy.Map, funcy.Filter, funcy.Sort*, etc.
+func Field[S, V any](name string) func(s S) V {
+	return func(s S) V { return reflect.ValueOf(s).FieldByName(name).Interface().(V) }
+}
---
+++ risky/risky_test.go
@@ -0,0 +1,15 @@
+package risky
+
+import (
+	"testing"
+
+	"github.com/mdwhatcott/funcy"
+	"github.com/mdwhatcott/funcy/internal/should"
+)
+
+func TestField(t *testing.T) {
+	type Thing struct{ ID int }
+	ID := Field[Thing, int]("ID")
+	sorted := funcy.Map(ID, funcy.SortAscending(ID, []Thing{{ID: 42}, {ID: 41}, {ID: 43}}))
+	should.So(t, sorted, should.Equal, []int{41, 42, 43})
+}
