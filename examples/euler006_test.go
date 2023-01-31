8a8414422e9c8bcc8f9ca156f4a85fcdb560e7f3
Michael Whatcott <mike@smarty.com>
2023-01-31 15:02:17 -0700

    Example: Euler #6

---
+++ examples/euler006_test.go
@@ -0,0 +1,17 @@
+package examples
+
+import (
+	"testing"
+
+	. "github.com/mdwhatcott/funcy"
+	"github.com/mdwhatcott/funcy/internal/should"
+)
+
+// https://projecteuler.net/problem=6
+func TestProjectEuler006(t *testing.T) {
+	should.So(t, Calculate006(10), should.Equal, 2640)
+	should.So(t, Calculate006(100), should.Equal, 25164150)
+}
+func Calculate006(n int) int { return SquareOfSums(n) - SumOfSquares(n) }
+func SquareOfSums(n int) int { return Square(Sum(Range(1, n+1))) }
+func SumOfSquares(n int) int { return Sum(Map(Square[int], Range(1, n+1))) }
