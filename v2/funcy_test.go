42bff8c1abe6a6c65f48cfd52329b1dd7170f1bc
Michael Whatcott <mdwhatcott@users.noreply.github.com>
2024-07-30 22:35:33 -0600

    Some rudimentary benchmarks demonstrating advantages of iter.Seq.

--- v2/funcy_test.go
+++ v2/funcy_test.go
@@ -8,6 +8,19 @@ import (
 	"github.com/mdwhatcott/funcy/v2/is"
 )
 
+func Benchmark(b *testing.B) {
+	b.ReportAllocs()
+	sum := 0
+	for i := 0; i < b.N; i++ {
+		for x := range Filter(is.Even[int], Range(0, 100_000)) {
+			sum += x
+		}
+	}
+	b.StopTimer()
+	b.Log(sum)
+	// BenchmarkBowlingv2-12    	    2684	    421081 ns/op	       0 B/op	       0 allocs/op
+}
+
 var (
 	_12     = []int{1, 2}
 	_123    = []int{1, 2, 3}
