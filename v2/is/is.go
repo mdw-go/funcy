2e58cd2c240c8d0bcc8fabdc0da75bd4ce1d8872
Michael Whatcott <mdwhatcott@users.noreply.github.com>
2024-08-05 20:10:00 -0600

    Tests

--- v2/is/is.go
+++ v2/is/is.go
@@ -1,12 +1,21 @@
 package is
 
-import "reflect"
+import (
+	"iter"
+	"reflect"
+)
 
+func abs[T Integer](t T) T {
+	if t < 0 {
+		return -t
+	}
+	return t
+}
 func Zero[T Number](t T) bool     { return t == 0 }
 func Positive[T Number](t T) bool { return t > 0 }
 func Negative[T Number](t T) bool { return t < 0 }
 func Even[T Integer](t T) bool    { return t%2 == 0 }
-func Odd[T Integer](t T) bool     { return t%2 == 1 }
+func Odd[T Integer](t T) bool     { return abs(t)%2 == 1 }
 func Nil[T any](t T) bool {
 	if any(t) == nil {
 		return true
@@ -17,6 +26,12 @@ func Nil[T any](t T) bool {
 	default:
 		return false
 	}
+}
+func Empty[V any](s iter.Seq[V]) bool {
+	for _ = range s {
+		return false
+	}
+	return true
 }
 
 type (
---
+++ v2/is/is_test.go
@@ -0,0 +1,80 @@
+package is_test
+
+import (
+	"iter"
+	"strings"
+	"testing"
+
+	"github.com/mdwhatcott/funcy/v2/internal/should"
+	"github.com/mdwhatcott/funcy/v2/is"
+)
+
+func seq[V any](s ...V) iter.Seq[V] {
+	return func(yield func(V) bool) {
+		for _, v := range s {
+			if !yield(v) {
+				return
+			}
+		}
+	}
+}
+func TestEmpty(t *testing.T) {
+	should.So(t, is.Empty(seq[int]()), should.BeTrue)
+	should.So(t, is.Empty(seq[int](1)), should.BeFalse)
+	should.So(t, is.Empty(seq[int](1, 2, 3)), should.BeFalse)
+}
+func TestZero(t *testing.T) {
+	should.So(t, is.Zero(0), should.BeTrue)
+	should.So(t, is.Zero(1), should.BeFalse)
+	should.So(t, is.Zero(0.0), should.BeTrue)
+	should.So(t, is.Zero(1.0), should.BeFalse)
+}
+func TestPositive(t *testing.T) {
+	should.So(t, is.Positive(-1.0), should.BeFalse)
+	should.So(t, is.Positive(-1), should.BeFalse)
+	should.So(t, is.Positive(0), should.BeFalse)
+	should.So(t, is.Positive(1), should.BeTrue)
+	should.So(t, is.Positive(1.0), should.BeTrue)
+}
+func TestNegative(t *testing.T) {
+	should.So(t, is.Negative(-1.0), should.BeTrue)
+	should.So(t, is.Negative(-1), should.BeTrue)
+	should.So(t, is.Negative(0), should.BeFalse)
+	should.So(t, is.Negative(1), should.BeFalse)
+	should.So(t, is.Negative(1.0), should.BeFalse)
+}
+func TestEven(t *testing.T) {
+	should.So(t, is.Even(-5), should.BeFalse)
+	should.So(t, is.Even(-4), should.BeTrue)
+	should.So(t, is.Even(-3), should.BeFalse)
+	should.So(t, is.Even(-2), should.BeTrue)
+	should.So(t, is.Even(-1), should.BeFalse)
+	should.So(t, is.Even(0), should.BeTrue)
+	should.So(t, is.Even(1), should.BeFalse)
+	should.So(t, is.Even(2), should.BeTrue)
+	should.So(t, is.Even(3), should.BeFalse)
+	should.So(t, is.Even(4), should.BeTrue)
+	should.So(t, is.Even(5), should.BeFalse)
+}
+func TestOdd(t *testing.T) {
+	should.So(t, is.Odd(-5), should.BeTrue)
+	should.So(t, is.Odd(-4), should.BeFalse)
+	should.So(t, is.Odd(-3), should.BeTrue)
+	should.So(t, is.Odd(-2), should.BeFalse)
+	should.So(t, is.Odd(-1), should.BeTrue)
+	should.So(t, is.Odd(0), should.BeFalse)
+	should.So(t, is.Odd(1), should.BeTrue)
+	should.So(t, is.Odd(2), should.BeFalse)
+	should.So(t, is.Odd(3), should.BeTrue)
+	should.So(t, is.Odd(4), should.BeFalse)
+	should.So(t, is.Odd(5), should.BeTrue)
+}
+func TestNil(t *testing.T) {
+	var a any
+	should.So(t, is.Nil(a), should.BeTrue)
+	should.So(t, is.Nil([]int(nil)), should.BeTrue)
+	var b *strings.Builder
+	should.So(t, is.Nil(b), should.BeTrue)
+	should.So(t, is.Nil(4), should.BeFalse)
+	should.So(t, is.Nil(strings.NewReader("HI")), should.BeFalse)
+}
