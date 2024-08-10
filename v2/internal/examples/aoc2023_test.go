dd52e42d53bf8a560beceb5cd5793bd9bd4a1428
Michael Whatcott <mdwhatcott@users.noreply.github.com>
2024-08-10 16:10:10 -0600

    Example: Advent of Code 2023 Day 1 Part 1

---
+++ v2/internal/examples/aoc2023_test.go
@@ -0,0 +1,34 @@
+package examples
+
+import (
+	"iter"
+	"strconv"
+	"testing"
+	"unicode"
+
+	. "github.com/mdwhatcott/funcy/v2"
+	"github.com/mdwhatcott/funcy/v2/internal/should"
+)
+
+func TestAdventOfCode2023Day1Part1(t *testing.T) {
+	// https://adventofcode.com/2023/day/1
+	should.So(t, Calibrate("1abc2"), should.Equal, 12)
+	should.So(t, Calibrate("pqr3stu8vwx"), should.Equal, 38)
+	should.So(t, Calibrate("a1b2c3d4e5f"), should.Equal, 15)
+	should.So(t, Calibrate("treb7uchet"), should.Equal, 77)
+	should.So(t, CalibrateAll(Variadic("1abc2", "pqr3stu8vwx", "a1b2c3d4e5f", "treb7uchet")),
+		should.Equal, 142)
+}
+func CalibrateAll(lines iter.Seq[string]) int {
+	return Sum(Map(Calibrate, lines))
+}
+func Calibrate(s string) int {
+	return ParseInt(string(Bookends(Filter(unicode.IsDigit, Iterate([]rune(s))))))
+}
+func Bookends(i iter.Seq[rune]) (result []rune) {
+	return append(result, First(i), Last(i))
+}
+func ParseInt(s string) int {
+	parsed, _ := strconv.Atoi(s)
+	return parsed
+}
