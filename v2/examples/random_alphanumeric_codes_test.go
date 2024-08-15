12f34ac67dbf7d1bf6c3ad74bd650d695cd7be9f
Michael Whatcott <mdwhatcott@users.noreply.github.com>
2024-08-15 08:52:34 -0600

    Completed additional example.

--- v2/examples/random_alphanumeric_codes_test.go
+++ v2/examples/random_alphanumeric_codes_test.go
@@ -1,3 +1,24 @@
 package examples
 
-// TODO: https://michaelwhatcott.com/generating-random-alphanumeric-codes-in-clojure/
+import (
+	"testing"
+
+	. "github.com/mdwhatcott/funcy/v2"
+)
+
+// https://michaelwhatcott.com/generating-random-alphanumeric-codes-in-clojure/
+
+func TestGenerate(t *testing.T) {
+	DoAll(
+		func(s string) { t.Log(s) },
+		Take(5, Repeatedly(Generate)),
+	)
+}
+func Generate() string { return string(Slice(Take(8, Repeatedly(randRune)))) }
+func randRune() rune   { return RandNth(alphaNumeric) }
+
+var alphaNumeric = Concat(
+	Range[rune]('a', 'z'),
+	Range[rune]('A', 'Z'),
+	Range[rune]('0', '9'),
+)
