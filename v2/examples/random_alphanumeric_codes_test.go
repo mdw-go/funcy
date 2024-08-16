09df360b09d330dcc2fb277b383019f0c13d2fa6
Michael Whatcott <mdwhatcott@users.noreply.github.com>
2024-08-15 19:36:46 -0600

    Simplify

--- v2/examples/random_alphanumeric_codes_test.go
+++ v2/examples/random_alphanumeric_codes_test.go
@@ -18,7 +18,7 @@ func Generate() string { return string(Slice(Take(8, Repeatedly(randRune)))) }
 func randRune() rune   { return RandNth(alphaNumeric) }
 
 var alphaNumeric = Concat(
-	Range[rune]('a', 'z'),
-	Range[rune]('A', 'Z'),
-	Range[rune]('0', '9'),
+	Range('a', 'z'),
+	Range('A', 'Z'),
+	Range('0', '9'),
 )
