5c856c30bc22fc1ade084378498396dda789963b
Michael Whatcott <mdwhatcott@users.noreply.github.com>
2024-08-17 10:28:06 -0600

    Common operations on numeric types.

---
+++ ranger/op/op.go
@@ -0,0 +1,21 @@
+package op
+
+import (
+	"math"
+
+	"github.com/mdwhatcott/funcy/ranger/is"
+)
+
+func Add[N is.Number](a, b N) N  { return a + b }
+func Sub[N is.Number](a, b N) N  { return a - b }
+func Div[N is.Number](a, b N) N  { return a / b }
+func Mul[N is.Number](a, b N) N  { return a * b }
+func Pow[N is.Number](a, b N) N  { return N(math.Pow(float64(a), float64(b))) }
+func Mod[N is.Integer](a, b N) N { return a % b }
+func Square[N is.Integer](n N) N { return Mul(n, n) }
+func Abs[N is.Number](n N) N {
+	if n < 0 {
+		return -n
+	}
+	return n
+}
