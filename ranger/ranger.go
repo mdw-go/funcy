823f87637544fb6b22393b5fd6db82be689d8cc9
Michael Whatcott <mdwhatcott@users.noreply.github.com>
2024-08-17 11:02:05 -0600

    Sort ranger package functions alphabetically.

--- ranger/ranger.go
+++ ranger/ranger.go
@@ -11,263 +11,14 @@ import (
 	"github.com/mdwhatcott/funcy/ranger/op"
 )
 
-func Variadic[V any](vs ...V) iter.Seq[V] {
-	return Iterator(vs)
-}
-func Iterator[S ~[]V, V any](s S) iter.Seq[V] {
-	return slices.Values(s)
-}
-func Slice[V any](seq iter.Seq[V]) (result []V) {
-	return slices.Collect(seq)
-}
-func Range[N is.Number](start, stop N) iter.Seq[N] {
-	var step N = 1
-	if stop < start {
-		step = -step
-	}
-	return RangeStep(start, stop, step)
-}
-func RangeStep[N is.Number](start, stop, step N) iter.Seq[N] {
-	return func(yield func(N) bool) {
-		for x := start; x != stop; x += step {
-			if !yield(x) {
-				return
-			}
-		}
-	}
-}
-func First[V any](s iter.Seq[V]) V {
-	return Nth(0, s)
-}
-func Nth[V any](n int, s iter.Seq[V]) V {
-	if n < 0 {
-		panic(fmt.Sprintf("runtime error: index out of range [%d]", n))
-	}
-	c := 0
-	for v := range s {
-		if c == n {
-			return v
-		}
-		c++
-	}
-	panic(fmt.Sprintf("runtime error: index out of range [%d] with length %d", n, c))
-}
-func RandNth[V any](s iter.Seq[V]) V {
-	return Nth(rand.N(Count(s)), s)
-}
-func Last[V any](s iter.Seq[V]) (result V) {
-	count := 0
-	for v := range s {
-		result = v
-		count++
-	}
-	if count > 0 {
-		return result
-	}
-	panic("runtime error: index out of range [0] with length 0")
-}
-func Take[V any](n int, s iter.Seq[V]) iter.Seq[V] {
-	return func(yield func(V) bool) {
-		count := 0
-		for v := range s {
-			if count >= n {
-				return
-			}
-			if !yield(v) {
-				return
-			}
-			count++
-		}
-	}
-}
-func TakeWhile[V any](pred func(V) bool, s iter.Seq[V]) iter.Seq[V] {
-	return func(yield func(V) bool) {
-		for v := range s {
-			if !pred(v) {
-				return
-			}
-			if !yield(v) {
-				return
-			}
-		}
-	}
-}
-func TakeLast[V any](n int, s iter.Seq[V]) iter.Seq[V] {
-	return func(yield func(V) bool) {
-		r := ring.New[V](n)
-		count := 0
-		for v := range s {
-			count++
-			r.Value = v
-			r = r.Next()
-		}
-		for x := 0; x < count; x++ {
-			r = r.Next()
-		}
-		for j := 0; j < count; j++ {
-			if !yield(r.Value) {
-				return
-			}
-			r = r.Next()
-		}
-	}
-}
-func Drop[V any](n int, s iter.Seq[V]) iter.Seq[V] {
-	return func(yield func(V) bool) {
-		count := 0
-		for v := range s {
-			if count < n {
-				count++
-				continue
-			}
-			if !yield(v) {
-				return
-			}
-		}
-	}
-}
-func DropWhile[V any](pred func(V) bool, s iter.Seq[V]) iter.Seq[V] {
-	return func(yield func(V) bool) {
-		dropping := true
-		for v := range s {
-			if dropping && pred(v) {
-				continue
-			} else if dropping {
-				dropping = false
-			}
-			if !yield(v) {
-				return
-			}
-		}
-	}
-}
-func DropLast[V any](n int, s iter.Seq[V]) iter.Seq[V] {
-	return Map2(func(a, _ V) V { return a }, s, Drop(n, s))
-}
-func Rest[V any](s iter.Seq[V]) iter.Seq[V] {
-	return Drop(1, s)
-}
-func Filter[V any](predicate func(V) bool, seq iter.Seq[V]) iter.Seq[V] {
-	return func(yield func(V) bool) {
-		for s := range seq {
-			if predicate(s) {
-				if !yield(s) {
-					return
-				}
-			}
-		}
-	}
-}
-func Remove[V any](predicate func(V) bool, seq iter.Seq[V]) iter.Seq[V] {
-	return Filter(Complement(predicate), seq)
-}
 func Complement[V any](predicate func(t V) bool) func(t V) bool {
 	return func(t V) bool { return !predicate(t) }
-}
-func Map[I, O any](f func(I) O, seq iter.Seq[I]) iter.Seq[O] {
-	return func(yield func(O) bool) {
-		for s := range seq {
-			if !yield(f(s)) {
-				return
-			}
-		}
-	}
-}
-func Map2[A, B, O any](f func(A, B) O, a iter.Seq[A], b iter.Seq[B]) iter.Seq[O] {
-	return func(yield func(O) bool) {
-		nextA, stopA := iter.Pull(a)
-		defer stopA()
-		nextB, stopB := iter.Pull(b)
-		defer stopB()
-		for {
-			aa, okA := nextA()
-			if !okA {
-				return
-			}
-			bb, okB := nextB()
-			if !okB {
-				return
-			}
-			if !yield(f(aa, bb)) {
-				return
-			}
-		}
-	}
-}
-func DoAll[V any](f func(V), seq iter.Seq[V]) {
-	for s := range seq {
-		f(s)
-	}
-}
-func Reduce[V any](calc func(a, b V) V, start V, seq iter.Seq[V]) (result V) {
-	return Last(Reductions(calc, start, seq))
-}
-func Reductions[V any](calc func(a, b V) V, start V, seq iter.Seq[V]) iter.Seq[V] {
-	return func(yield func(V) bool) {
-		result := start
-		for next := range seq {
-			result = calc(result, next)
-			if !yield(result) {
-				return
-			}
-		}
-	}
-}
-func Repeat[V any](v V) iter.Seq[V] {
-	return Repeatedly(func() V { return v })
-}
-func RepeatN[V any](n int, v V) iter.Seq[V] {
-	return Take(n, Repeatedly(func() V { return v }))
-}
-func Repeatedly[V any](v func() V) iter.Seq[V] {
-	return func(yield func(V) bool) {
-		for yield(v()) {
-		}
-	}
 }
 func Concat[V any](all ...iter.Seq[V]) iter.Seq[V] {
 	return func(yield func(V) bool) {
 		for _, seq := range all {
 			for v := range seq {
 				if !yield(v) {
-					return
-				}
-			}
-		}
-	}
-}
-func Sum[N is.Number](seq iter.Seq[N]) N {
-	return Reduce(op.Add[N], N(0), seq)
-}
-func Product[N is.Number](seq iter.Seq[N]) N {
-	return Reduce(op.Mul[N], N(1), seq)
-}
-func Nest[V any](matrix [][]V) iter.Seq[iter.Seq[V]] {
-	return func(yield func(iter.Seq[V]) bool) {
-		for _, row := range matrix {
-			_ = yield(Iterator(row))
-		}
-	}
-}
-func Partition[V any](chunkLength, stride int, seq iter.Seq[V]) iter.Seq[iter.Seq[V]] {
-	return func(yield func(iter.Seq[V]) bool) {
-		for {
-			chunk := Take(chunkLength, seq)
-			if Count(chunk) < chunkLength {
-				return
-			}
-			if !yield(chunk) {
-				return
-			}
-			seq = Drop(stride, seq)
-		}
-	}
-}
-func Flatten[V any](matrix iter.Seq[iter.Seq[V]]) iter.Seq[V] {
-	return func(yield func(V) bool) {
-		for row := range matrix {
-			for cell := range row {
-				if !yield(cell) {
 					return
 				}
 			}
@@ -290,6 +41,90 @@ func Cycle[V any](seq iter.Seq[V]) iter.Seq[V] {
 			}
 		}
 	}
+}
+func DoAll[V any](f func(V), seq iter.Seq[V]) {
+	for s := range seq {
+		f(s)
+	}
+}
+func Drop[V any](n int, s iter.Seq[V]) iter.Seq[V] {
+	return func(yield func(V) bool) {
+		count := 0
+		for v := range s {
+			if count < n {
+				count++
+				continue
+			}
+			if !yield(v) {
+				return
+			}
+		}
+	}
+}
+func DropLast[V any](n int, s iter.Seq[V]) iter.Seq[V] {
+	return Map2(func(a, _ V) V { return a }, s, Drop(n, s))
+}
+func DropWhile[V any](pred func(V) bool, s iter.Seq[V]) iter.Seq[V] {
+	return func(yield func(V) bool) {
+		dropping := true
+		for v := range s {
+			if dropping && pred(v) {
+				continue
+			} else if dropping {
+				dropping = false
+			}
+			if !yield(v) {
+				return
+			}
+		}
+	}
+}
+func Filter[V any](predicate func(V) bool, seq iter.Seq[V]) iter.Seq[V] {
+	return func(yield func(V) bool) {
+		for s := range seq {
+			if predicate(s) {
+				if !yield(s) {
+					return
+				}
+			}
+		}
+	}
+}
+func First[V any](s iter.Seq[V]) V {
+	return Nth(0, s)
+}
+func Flatten[V any](matrix iter.Seq[iter.Seq[V]]) iter.Seq[V] {
+	return func(yield func(V) bool) {
+		for row := range matrix {
+			for cell := range row {
+				if !yield(cell) {
+					return
+				}
+			}
+		}
+	}
+}
+func Frequencies[V comparable](seq iter.Seq[V]) map[V]int {
+	result := make(map[V]int)
+	for s := range seq {
+		result[s]++
+	}
+	return result
+}
+func GroupBy[K comparable, V any](f func(V) K, seq iter.Seq[V]) map[K][]V {
+	result := make(map[K][]V)
+	for v := range seq {
+		key := f(v)
+		result[key] = append(result[key], v)
+	}
+	return result
+}
+func IndexBy[K comparable, V any](f func(V) K, seq iter.Seq[V]) map[K]V {
+	result := make(map[K]V)
+	for v := range seq {
+		result[f(v)] = v
+	}
+	return result
 }
 func Interleave[V any](a, b iter.Seq[V]) iter.Seq[V] {
 	return func(yield func(V) bool) {
@@ -328,27 +163,210 @@ func Iterate[V any](f func(V) V, v V) iter.Seq[V] {
 		}
 	}
 }
-func Frequencies[V comparable](seq iter.Seq[V]) map[V]int {
-	result := make(map[V]int)
-	for s := range seq {
-		result[s]++
+func Iterator[S ~[]V, V any](s S) iter.Seq[V] {
+	return slices.Values(s)
+}
+func Last[V any](s iter.Seq[V]) (result V) {
+	count := 0
+	for v := range s {
+		result = v
+		count++
+	}
+	if count > 0 {
+		return result
+	}
+	panic("runtime error: index out of range [0] with length 0")
+}
+func Map[I, O any](f func(I) O, seq iter.Seq[I]) iter.Seq[O] {
+	return func(yield func(O) bool) {
+		for s := range seq {
+			if !yield(f(s)) {
+				return
+			}
+		}
+	}
+}
+func Map2[A, B, O any](f func(A, B) O, a iter.Seq[A], b iter.Seq[B]) iter.Seq[O] {
+	return func(yield func(O) bool) {
+		nextA, stopA := iter.Pull(a)
+		defer stopA()
+		nextB, stopB := iter.Pull(b)
+		defer stopB()
+		for {
+			aa, okA := nextA()
+			if !okA {
+				return
+			}
+			bb, okB := nextB()
+			if !okB {
+				return
+			}
+			if !yield(f(aa, bb)) {
+				return
+			}
+		}
+	}
+}
+func Max[V is.LessThan](s iter.Seq[V]) (result V) {
+	result = First(s)
+	for s := range Rest(s) {
+		if s > result {
+			result = s
+		}
 	}
 	return result
 }
-func IndexBy[K comparable, V any](f func(V) K, seq iter.Seq[V]) map[K]V {
-	result := make(map[K]V)
-	for v := range seq {
-		result[f(v)] = v
+func Min[V is.LessThan](s iter.Seq[V]) (result V) {
+	result = First(s)
+	for s := range Rest(s) {
+		if s < result {
+			result = s
+		}
 	}
 	return result
 }
-func GroupBy[K comparable, V any](f func(V) K, seq iter.Seq[V]) map[K][]V {
-	result := make(map[K][]V)
-	for v := range seq {
-		key := f(v)
-		result[key] = append(result[key], v)
+func Nest[V any](matrix [][]V) iter.Seq[iter.Seq[V]] {
+	return func(yield func(iter.Seq[V]) bool) {
+		for _, row := range matrix {
+			_ = yield(Iterator(row))
+		}
 	}
-	return result
+}
+func Nth[V any](n int, s iter.Seq[V]) V {
+	if n < 0 {
+		panic(fmt.Sprintf("runtime error: index out of range [%d]", n))
+	}
+	c := 0
+	for v := range s {
+		if c == n {
+			return v
+		}
+		c++
+	}
+	panic(fmt.Sprintf("runtime error: index out of range [%d] with length %d", n, c))
+}
+func Partition[V any](chunkLength, stride int, seq iter.Seq[V]) iter.Seq[iter.Seq[V]] {
+	return func(yield func(iter.Seq[V]) bool) {
+		for {
+			chunk := Take(chunkLength, seq)
+			if Count(chunk) < chunkLength {
+				return
+			}
+			if !yield(chunk) {
+				return
+			}
+			seq = Drop(stride, seq)
+		}
+	}
+}
+func Product[N is.Number](seq iter.Seq[N]) N {
+	return Reduce(op.Mul[N], N(1), seq)
+}
+func RandNth[V any](s iter.Seq[V]) V {
+	return Nth(rand.N(Count(s)), s)
+}
+func Range[N is.Number](start, stop N) iter.Seq[N] {
+	var step N = 1
+	if stop < start {
+		step = -step
+	}
+	return RangeStep(start, stop, step)
+}
+func RangeStep[N is.Number](start, stop, step N) iter.Seq[N] {
+	return func(yield func(N) bool) {
+		for x := start; x != stop; x += step {
+			if !yield(x) {
+				return
+			}
+		}
+	}
+}
+func Reduce[V any](calc func(a, b V) V, start V, seq iter.Seq[V]) (result V) {
+	return Last(Reductions(calc, start, seq))
+}
+func Reductions[V any](calc func(a, b V) V, start V, seq iter.Seq[V]) iter.Seq[V] {
+	return func(yield func(V) bool) {
+		result := start
+		for next := range seq {
+			result = calc(result, next)
+			if !yield(result) {
+				return
+			}
+		}
+	}
+}
+func Remove[V any](predicate func(V) bool, seq iter.Seq[V]) iter.Seq[V] {
+	return Filter(Complement(predicate), seq)
+}
+func Repeat[V any](v V) iter.Seq[V] {
+	return Repeatedly(func() V { return v })
+}
+func RepeatN[V any](n int, v V) iter.Seq[V] {
+	return Take(n, Repeatedly(func() V { return v }))
+}
+func Repeatedly[V any](v func() V) iter.Seq[V] {
+	return func(yield func(V) bool) {
+		for yield(v()) {
+		}
+	}
+}
+func Rest[V any](s iter.Seq[V]) iter.Seq[V] {
+	return Drop(1, s)
+}
+func Slice[V any](seq iter.Seq[V]) (result []V) {
+	return slices.Collect(seq)
+}
+func Sum[N is.Number](seq iter.Seq[N]) N {
+	return Reduce(op.Add[N], N(0), seq)
+}
+func Take[V any](n int, s iter.Seq[V]) iter.Seq[V] {
+	return func(yield func(V) bool) {
+		count := 0
+		for v := range s {
+			if count >= n {
+				return
+			}
+			if !yield(v) {
+				return
+			}
+			count++
+		}
+	}
+}
+func TakeLast[V any](n int, s iter.Seq[V]) iter.Seq[V] {
+	return func(yield func(V) bool) {
+		r := ring.New[V](n)
+		count := 0
+		for v := range s {
+			count++
+			r.Value = v
+			r = r.Next()
+		}
+		for x := 0; x < count; x++ {
+			r = r.Next()
+		}
+		for j := 0; j < count; j++ {
+			if !yield(r.Value) {
+				return
+			}
+			r = r.Next()
+		}
+	}
+}
+func TakeWhile[V any](pred func(V) bool, s iter.Seq[V]) iter.Seq[V] {
+	return func(yield func(V) bool) {
+		for v := range s {
+			if !pred(v) {
+				return
+			}
+			if !yield(v) {
+				return
+			}
+		}
+	}
+}
+func Variadic[V any](vs ...V) iter.Seq[V] {
+	return Iterator(vs)
 }
 func ZipMap[K comparable, V any](k iter.Seq[K], v iter.Seq[V]) map[K]V {
 	nextA, stopA := iter.Pull(k)
@@ -366,24 +384,6 @@ func ZipMap[K comparable, V any](k iter.Seq[K], v iter.Seq[V]) map[K]V {
 			break
 		}
 		result[aa] = bb
-	}
-	return result
-}
-func Min[V is.LessThan](s iter.Seq[V]) (result V) {
-	result = First(s)
-	for s := range Rest(s) {
-		if s < result {
-			result = s
-		}
-	}
-	return result
-}
-func Max[V is.LessThan](s iter.Seq[V]) (result V) {
-	result = First(s)
-	for s := range Rest(s) {
-		if s > result {
-			result = s
-		}
 	}
 	return result
 }
--- ranger/ranger_test.go
+++ ranger/ranger_test.go
@@ -24,30 +24,25 @@ var (
 	_nil       = []int(nil)
 )
 
-func TestRange(t *testing.T) {
-	should.So(t, Slice(Range(1, 10)), should.Equal, _123456789)
-	should.So(t, Slice(Range(9, 0)), should.Equal, _987654321)
-	should.So(t, Slice(RangeStep(9, 0, -1)), should.Equal, _987654321)
+func TestConcat(t *testing.T) {
+	should.So(t,
+		Slice(Take(12, Concat(Range(0, 5), Range(5, 10), Range(10, 15)))), should.Equal,
+		Slice(Range(0, 12)),
+	)
 }
-func TestIterator(t *testing.T) {
-	should.So(t, Slice(Iterator(_123)), should.Equal, _123)
-	should.So(t, Slice(Take(2, Iterator(_123))), should.Equal, _12)
-	should.So(t, Slice(Variadic(1, 2, 3)), should.Equal, _123)
+func TestCount(t *testing.T) {
+	should.So(t, Count(Range(0, 20)), should.Equal, 20)
 }
-func TestTake(t *testing.T) {
-	should.So(t, Slice(Take(4, Range(0, 10))), should.Equal, _0123)
-	should.So(t, Slice(Take(8, Range(1, 5))), should.Equal, _1234)
-	should.So(t, Slice(Take(8, Range(1, 5))), should.Equal, _1234)
-	should.So(t, Slice(Take(1, Range(0, 0))), should.Equal, _nil)
-	should.So(t, Slice(Take(2, Take(3, Range(1, 10)))), should.Equal, _12)
+func TestCycle(t *testing.T) {
+	should.So(t, Slice(Take(9, Cycle(Range(0, 2)))), should.Equal, []int{0, 1, 0, 1, 0, 1, 0, 1, 0})
 }
-func TestTakeWhile(t *testing.T) {
-	should.So(t, Slice(Take(4, TakeWhile(is.Even[int], Iterator([]int{0, 2, 4, 6, 8, 1, 3, 5, 7})))), should.Equal, _0246)
-	should.So(t, Slice(TakeWhile(is.Even[int], Iterator([]int{1, 3, 5, 7, 0, 2, 4, 6, 8}))), should.Equal, _nil)
-}
-func TestTakeLast(t *testing.T) {
-	should.So(t, Slice(TakeLast(20, Range(0, 10))), should.Equal, Slice(Range(0, 10)))
-	should.So(t, Slice(Take(4, TakeLast(5, Range(0, 10)))), should.Equal, []int{5, 6, 7, 8})
+func TestDoAll(t *testing.T) {
+	var all []int
+	store := func(a int) {
+		all = append(all, a)
+	}
+	DoAll(store, Range(1, 10))
+	should.So(t, all, should.Equal, _123456789)
 }
 func TestDrop(t *testing.T) {
 	should.So(t, Slice(Drop(4, Range(0, 10))), should.Equal, _456789)
@@ -55,76 +50,20 @@ func TestDrop(t *testing.T) {
 	should.So(t, Slice(Drop(10, Range(0, 0))), should.Equal, _nil)
 	should.So(t, Slice(Drop(1, Range(0, 0))), should.Equal, _nil)
 }
-func TestDropWhile(t *testing.T) {
-	should.So(t, Slice(Take(3, DropWhile(is.Even[int], Iterator([]int{0, 2, 4, 6, 8, 1, 3, 5, 7})))), should.Equal, _135)
-	should.So(t, Slice(DropWhile(is.Even[int], Range(1, 10))), should.Equal, Slice(Range(1, 10)))
-}
 func TestDropLast(t *testing.T) {
 	should.So(t, Slice(Take(5, DropLast(4, Range(1, 11)))), should.Equal, _12345)
 }
-func TestFirst(t *testing.T) {
-	should.So(t, func() { First(Take(0, Range(0, 10))) }, should.Panic)
-	should.So(t, First(Drop(1, Range(1, 10))), should.Equal, 2)
-}
-func TestNth(t *testing.T) {
-	should.So(t, func() { Nth(-1, Iterator(_1234)) }, should.Panic)
-	should.So(t, Nth(2, Iterator(_1234)), should.Equal, 3)
-}
-func TestRandNth(t *testing.T) {
-	should.So(t, RandNth(Variadic(42)), should.Equal, 42)
-	var values []int
-	for range 100 {
-		nth := RandNth(Iterator(_123456789))
-		values = append(values, nth)
-		should.So(t, nth, should.BeGreaterThan, 0)
-		should.So(t, nth, should.BeLessThan, 10)
-	}
-	freq := Frequencies(Iterator(values))
-	for _, n := range _123456789 {
-		should.So(t, freq[n], should.BeGreaterThan, 0)
-	}
-}
-func TestLast(t *testing.T) {
-	should.So(t, func() { Last(Take(0, Range(0, 10))) }, should.Panic)
-	should.So(t, Last(Take(3, Range(1, 10))), should.Equal, 3)
-}
-func TestRest(t *testing.T) {
-	should.So(t, Slice(Take(3, Rest(Range(1, 10)))), should.Equal, Slice(Range(2, 5)))
-	should.So(t, Slice(Take(3, Rest(Range(0, 0)))), should.Equal, _nil)
+func TestDropWhile(t *testing.T) {
+	should.So(t, Slice(Take(3, DropWhile(is.Even[int], Iterator([]int{0, 2, 4, 6, 8, 1, 3, 5, 7})))), should.Equal, _135)
+	should.So(t, Slice(DropWhile(is.Even[int], Range(1, 10))), should.Equal, Slice(Range(1, 10)))
 }
 func TestFilter(t *testing.T) {
 	should.So(t, Slice(Take(4, Filter(is.Even[int], Range(0, 10)))), should.Equal, _0246)
 	should.So(t, Slice(Take(4, Remove(is.Even[int], Range(0, 10)))), should.Equal, _1357)
 }
-func TestMap(t *testing.T) {
-	square := func(n int) int64 { return int64(n * n) }
-	upper := func(r rune) string { return strings.ToUpper(string(r)) }
-	should.So(t, Slice(Take(5, Map(square, Range(2, 10)))), should.Equal, []int64{4, 9, 16, 25, 36})
-	should.So(t, Slice(Map(upper, Iterator([]rune("asdf")))), should.Equal, []string{"A", "S", "D", "F"})
-}
-func TestMap2(t *testing.T) {
-	add := func(a int, b int) int { return a + b }
-	should.So(t, Slice(Take(5, Map2(add, Range(0, 10), RepeatN(10, 1)))), should.Equal, _12345)
-	should.So(t, Slice(Map2(add, Range(0, 1), RepeatN(10, 1))), should.Equal, []int{1})
-	should.So(t, Slice(Map2(add, Range(0, 10), RepeatN(2, 1))), should.Equal, []int{1, 2})
-	should.So(t, Slice(Map2(add, Range(0, 0), RepeatN(1, 1))), should.Equal, []int(nil))
-	should.So(t, Slice(Map2(add, Range(0, 1), RepeatN(0, 1))), should.Equal, []int(nil))
-}
-func TestReduce(t *testing.T) {
-	add := func(a, b int) int { return a + b }
-	should.So(t, Reduce(add, 0, Range(1, 6)), should.Equal, 15)
-}
-func TestRepeat(t *testing.T) {
-	should.So(t,
-		Slice(Take(10, RepeatN(20, 1))), should.Equal,
-		[]int{1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-	)
-}
-func TestConcat(t *testing.T) {
-	should.So(t,
-		Slice(Take(12, Concat(Range(0, 5), Range(5, 10), Range(10, 15)))), should.Equal,
-		Slice(Range(0, 12)),
-	)
+func TestFirst(t *testing.T) {
+	should.So(t, func() { First(Take(0, Range(0, 10))) }, should.Panic)
+	should.So(t, First(Drop(1, Range(1, 10))), should.Equal, 2)
 }
 func TestFlatten(t *testing.T) {
 	should.So(t, Slice(Take(12, Flatten(Nest([][]int{
@@ -133,22 +72,29 @@ func TestFlatten(t *testing.T) {
 		{10, 11, 12, 13, 14},
 	})))), should.Equal, Slice(Range(0, 12)))
 }
-func TestPartition(t *testing.T) {
-	should.So(t, Slice(Map(Sum[int], Partition(3, 3, Range(1, 10)))), should.Equal, []int{6, 15, 24})
-	should.So(t, Slice(Map(Sum[int], Take(2, Partition(3, 3, Range(1, 10))))), should.Equal, []int{6, 15})
-	should.So(t, Slice(Map(Sum[int], Partition(3, 1, Range(1, 5)))), should.Equal, []int{6, 9})
+func TestFrequencies(t *testing.T) {
+	should.So(t, Frequencies(Variadic(1, 1, 2, 2, 2, 3, 4, 4)), should.Equal, map[int]int{
+		1: 2,
+		2: 3,
+		3: 1,
+		4: 2,
+	})
 }
-func TestSum(t *testing.T) {
-	should.So(t, Sum(Range(1, 6)), should.Equal, 1+2+3+4+5)
+func TestGroupBy(t *testing.T) {
+	should.So(t, GroupBy(strconv.Itoa, Concat(Range(0, 5), Range(1, 4))), should.Equal,
+		map[string][]int{
+			"0": {0},
+			"1": {1, 1},
+			"2": {2, 2},
+			"3": {3, 3},
+			"4": {4},
+		},
+	)
 }
-func TestProduct(t *testing.T) {
-	should.So(t, Product(Range(1, 6)), should.Equal, 1*2*3*4*5)
-}
-func TestCount(t *testing.T) {
-	should.So(t, Count(Range(0, 20)), should.Equal, 20)
-}
-func TestCycle(t *testing.T) {
-	should.So(t, Slice(Take(9, Cycle(Range(0, 2)))), should.Equal, []int{0, 1, 0, 1, 0, 1, 0, 1, 0})
+func TestIndexBy(t *testing.T) {
+	should.So(t, IndexBy(strconv.Itoa, Range(0, 5)), should.Equal,
+		map[string]int{"0": 0, "1": 1, "2": 2, "3": 3, "4": 4},
+	)
 }
 func TestInterleave(t *testing.T) {
 	should.So(t, Slice(Take(5, Interleave(Range(0, 10), Range(10, 20)))), should.Equal, []int{0, 10, 1, 11, 2})
@@ -169,8 +115,95 @@ func TestInterpose(t *testing.T) {
 		1, -1,
 	})
 }
-func TestRepeatedly(t *testing.T) {
-	should.So(t, Slice(Take(5, Repeatedly(func() int { return 3 }))), should.Equal, []int{3, 3, 3, 3, 3})
+func TestIterate(t *testing.T) {
+	inc := func(a int) int { return a + 1 }
+	should.So(t, Slice(Take(5, Iterate(inc, 0))), should.Equal, Slice(Range(1, 6)))
+}
+func TestIterator(t *testing.T) {
+	should.So(t, Slice(Iterator(_123)), should.Equal, _123)
+	should.So(t, Slice(Take(2, Iterator(_123))), should.Equal, _12)
+	should.So(t, Slice(Variadic(1, 2, 3)), should.Equal, _123)
+}
+func TestLast(t *testing.T) {
+	should.So(t, func() { Last(Take(0, Range(0, 10))) }, should.Panic)
+	should.So(t, Last(Take(3, Range(1, 10))), should.Equal, 3)
+}
+func TestMap(t *testing.T) {
+	square := func(n int) int64 { return int64(n * n) }
+	upper := func(r rune) string { return strings.ToUpper(string(r)) }
+	should.So(t, Slice(Take(5, Map(square, Range(2, 10)))), should.Equal, []int64{4, 9, 16, 25, 36})
+	should.So(t, Slice(Map(upper, Iterator([]rune("asdf")))), should.Equal, []string{"A", "S", "D", "F"})
+}
+func TestMap2(t *testing.T) {
+	add := func(a int, b int) int { return a + b }
+	should.So(t, Slice(Take(5, Map2(add, Range(0, 10), RepeatN(10, 1)))), should.Equal, _12345)
+	should.So(t, Slice(Map2(add, Range(0, 1), RepeatN(10, 1))), should.Equal, []int{1})
+	should.So(t, Slice(Map2(add, Range(0, 10), RepeatN(2, 1))), should.Equal, []int{1, 2})
+	should.So(t, Slice(Map2(add, Range(0, 0), RepeatN(1, 1))), should.Equal, []int(nil))
+	should.So(t, Slice(Map2(add, Range(0, 1), RepeatN(0, 1))), should.Equal, []int(nil))
+}
+func TestMax(t *testing.T) {
+	should.So(t, Max(Range(4, 20)), should.Equal, 19)
+	should.So(t, func() { Max(Range(0, 0)) }, should.Panic)
+	should.So(t, Max(Variadic(1, 6, -2, 3, 42, 7)), should.Equal, 42)
+}
+func TestMin(t *testing.T) {
+	should.So(t, Min(Range(4, 20)), should.Equal, 4)
+	should.So(t, func() { Min(Range(0, 0)) }, should.Panic)
+	should.So(t, Min(Variadic(1, 6, -2, 3, 42)), should.Equal, -2)
+}
+func TestNth(t *testing.T) {
+	should.So(t, func() { Nth(-1, Iterator(_1234)) }, should.Panic)
+	should.So(t, Nth(2, Iterator(_1234)), should.Equal, 3)
+}
+func TestPartition(t *testing.T) {
+	should.So(t, Slice(Map(Sum[int], Partition(3, 3, Range(1, 10)))), should.Equal, []int{6, 15, 24})
+	should.So(t, Slice(Map(Sum[int], Take(2, Partition(3, 3, Range(1, 10))))), should.Equal, []int{6, 15})
+	should.So(t, Slice(Map(Sum[int], Partition(3, 1, Range(1, 5)))), should.Equal, []int{6, 9})
+}
+func TestProduct(t *testing.T) {
+	should.So(t, Product(Range(1, 6)), should.Equal, 1*2*3*4*5)
+}
+func TestSum(t *testing.T) {
+	should.So(t, Sum(Range(1, 6)), should.Equal, 1+2+3+4+5)
+}
+func TestTake(t *testing.T) {
+	should.So(t, Slice(Take(4, Range(0, 10))), should.Equal, _0123)
+	should.So(t, Slice(Take(8, Range(1, 5))), should.Equal, _1234)
+	should.So(t, Slice(Take(8, Range(1, 5))), should.Equal, _1234)
+	should.So(t, Slice(Take(1, Range(0, 0))), should.Equal, _nil)
+	should.So(t, Slice(Take(2, Take(3, Range(1, 10)))), should.Equal, _12)
+}
+func TestTakeLast(t *testing.T) {
+	should.So(t, Slice(TakeLast(20, Range(0, 10))), should.Equal, Slice(Range(0, 10)))
+	should.So(t, Slice(Take(4, TakeLast(5, Range(0, 10)))), should.Equal, []int{5, 6, 7, 8})
+}
+func TestTakeWhile(t *testing.T) {
+	should.So(t, Slice(Take(4, TakeWhile(is.Even[int], Iterator([]int{0, 2, 4, 6, 8, 1, 3, 5, 7})))), should.Equal, _0246)
+	should.So(t, Slice(TakeWhile(is.Even[int], Iterator([]int{1, 3, 5, 7, 0, 2, 4, 6, 8}))), should.Equal, _nil)
+}
+func TestRandNth(t *testing.T) {
+	should.So(t, RandNth(Variadic(42)), should.Equal, 42)
+	var values []int
+	for range 100 {
+		nth := RandNth(Iterator(_123456789))
+		values = append(values, nth)
+		should.So(t, nth, should.BeGreaterThan, 0)
+		should.So(t, nth, should.BeLessThan, 10)
+	}
+	freq := Frequencies(Iterator(values))
+	for _, n := range _123456789 {
+		should.So(t, freq[n], should.BeGreaterThan, 0)
+	}
+}
+func TestRange(t *testing.T) {
+	should.So(t, Slice(Range(1, 10)), should.Equal, _123456789)
+	should.So(t, Slice(Range(9, 0)), should.Equal, _987654321)
+	should.So(t, Slice(RangeStep(9, 0, -1)), should.Equal, _987654321)
+}
+func TestReduce(t *testing.T) {
+	add := func(a, b int) int { return a + b }
+	should.So(t, Reduce(add, 0, Range(1, 6)), should.Equal, 15)
 }
 func TestReductions(t *testing.T) {
 	add := func(a int, b int) int { return a + b }
@@ -178,41 +211,18 @@ func TestReductions(t *testing.T) {
 	should.So(t, Slice(Reductions(add, 0, Range(1, 6))), should.Equal, []int{1, 3, 6, 10, 15})
 	should.So(t, Last(Reductions(add, 0, Range(1, 6))), should.Equal, Reduce(add, 0, Range(1, 6)))
 }
-func TestIterate(t *testing.T) {
-	inc := func(a int) int { return a + 1 }
-	should.So(t, Slice(Take(5, Iterate(inc, 0))), should.Equal, Slice(Range(1, 6)))
-}
-func TestFrequencies(t *testing.T) {
-	should.So(t, Frequencies(Variadic(1, 1, 2, 2, 2, 3, 4, 4)), should.Equal, map[int]int{
-		1: 2,
-		2: 3,
-		3: 1,
-		4: 2,
-	})
-}
-func TestDoAll(t *testing.T) {
-	var all []int
-	store := func(a int) {
-		all = append(all, a)
-	}
-	DoAll(store, Range(1, 10))
-	should.So(t, all, should.Equal, _123456789)
-}
-func TestIndexBy(t *testing.T) {
-	should.So(t, IndexBy(strconv.Itoa, Range(0, 5)), should.Equal,
-		map[string]int{"0": 0, "1": 1, "2": 2, "3": 3, "4": 4},
+func TestRepeat(t *testing.T) {
+	should.So(t,
+		Slice(Take(10, RepeatN(20, 1))), should.Equal,
+		[]int{1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
 	)
 }
-func TestGroupBy(t *testing.T) {
-	should.So(t, GroupBy(strconv.Itoa, Concat(Range(0, 5), Range(1, 4))), should.Equal,
-		map[string][]int{
-			"0": {0},
-			"1": {1, 1},
-			"2": {2, 2},
-			"3": {3, 3},
-			"4": {4},
-		},
-	)
+func TestRepeatedly(t *testing.T) {
+	should.So(t, Slice(Take(5, Repeatedly(func() int { return 3 }))), should.Equal, []int{3, 3, 3, 3, 3})
+}
+func TestRest(t *testing.T) {
+	should.So(t, Slice(Take(3, Rest(Range(1, 10)))), should.Equal, Slice(Range(2, 5)))
+	should.So(t, Slice(Take(3, Rest(Range(0, 0)))), should.Equal, _nil)
 }
 func TestZipMap(t *testing.T) {
 	should.So(t, ZipMap(Range(0, 5), Range(10, 15)), should.Equal, map[int]int{
@@ -236,14 +246,4 @@ func TestZipMap(t *testing.T) {
 		3: 13,
 		4: 14,
 	})
-}
-func TestMin(t *testing.T) {
-	should.So(t, Min(Range(4, 20)), should.Equal, 4)
-	should.So(t, func() { Min(Range(0, 0)) }, should.Panic)
-	should.So(t, Min(Variadic(1, 6, -2, 3, 42)), should.Equal, -2)
-}
-func TestMax(t *testing.T) {
-	should.So(t, Max(Range(4, 20)), should.Equal, 19)
-	should.So(t, func() { Max(Range(0, 0)) }, should.Panic)
-	should.So(t, Max(Variadic(1, 6, -2, 3, 42, 7)), should.Equal, 42)
 }
